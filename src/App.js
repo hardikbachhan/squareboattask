23aafa143c6be72af309aa14af725eb4e670f36c
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-11-23 13:28:44 +0530

    create jobs panel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Login from "./components/login/Login";
+import Jobs from "./components/postedjobs/Jobs";
 
 function App() {
     return (
         <div className="body">
             <Navbar />
-            <Login />
+            {/* <Login /> */}
+            <Jobs />
         </div>
     );
 }
---
+++ src/components/postedjobs/Jobs.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./jobs.css";
+
+function Jobs() {
+  return (
+    <div className="jobs-portal">
+      <div className="compartment-1"></div>
+      <div className="compartment-2"></div>
+      <div className="jobs-panel">
+
+      </div>
+    </div>
+  );
+}
+
+export default Jobs;
