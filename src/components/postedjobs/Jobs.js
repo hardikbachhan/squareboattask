a7601f8bc4d057f1cc4d1cdad3e0c9ba2d60a9e6
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-11-23 15:29:17 +0530

    successfully fetch job details using recruiter jwt

--- src/components/postedjobs/Jobs.js
+++ src/components/postedjobs/Jobs.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Job from "./jobdetails/Job";
 import "./jobs.css";
 
 function Jobs() {
+  const [jobs, setJobs] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const fetchJobDetails = async (change) => {
+    let newPage = page + change;
+
+    if (newPage >= 1) {
+      let token = localStorage.getItem("token");
+      let url = "https://jobs-api.squareboat.info/api/v1/recruiters/jobs";
+      url += `?page=${newPage}`;
+      const res = await fetch(url, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      });
+      const jobDetails = await res.json();
+      console.log(jobDetails);
+    }
+  };
+
+  useEffect(() => {
+    console.log("useEffect called");
+    fetchJobDetails(0);
+    console.log("useEffect finished");
+  }, []);
+
   return (
     <div className="jobs-portal">
       <div className="compartment-1"></div>
       <div className="compartment-2"></div>
       <div className="jobs-panel">
-
+        {jobs.map((jobObj) => (
+          <Job key={jobObj.id} />
+        ))}
       </div>
     </div>
   );
